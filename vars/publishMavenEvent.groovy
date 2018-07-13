07f7c4036fdcef43e44a4452015c68f3a0ce8523
Brent McConnell <bmcconnell@cloudbees.com>
2018-07-13 16:47:49 -0400

    Update publishMavenEvent.groovy

--- vars/publishMavenEvent.groovy
+++ vars/publishMavenEvent.groovy
@@ -1,33 +1,34 @@
 def call() {
     echo "Publishing event for Maven build"
-    def jsonStr = ""
+    def jsonStr = """
+    {
+        "event":"%s",
+        "artifactId": "%s",
+        "groupId": "%s",
+        "version": "%s",
+        "package": "%s"
+    }
+    """
     try {
         def pom = readMavenPom file: 'pom.xml'
         POM_VERSION = pom.version
         POM_ARTIFACT = pom.artifactId
         POM_GROUP = pom.groupId
         POM_PACKAGE = pom.packaging
+        EVENT_STR = "$POM_GROUP:$POM_ARTIFACT:$POM_VERSION:$POM_PACKAGE"
         echo "Building $JOB_NAME"
         echo "Building Version=$POM_VERSION"
         echo "Building Artifact=$POM_ARTIFACT"
         echo "Building Package=$POM_PACKAGE"
         echo "Building Group=$POM_GROUP"
-        jsonStr = """
-    {
-        "event":"$POM_GROUP:$POM_ARTIFACT:$POM_VERSION:$POM_PACKAGE",
-        "artifactId": "$POM_ARTIFACT",
-        "groupId": "$POM_GROUP",
-        "version": "$POM_VERSION",
-        "package": "$POM_PACKAGE"
-    }
-    """
+        
     } catch (e) {
         echo "Cannot access pom.xml file"
         throw e
     }
     
     try {
-        publishEvent event: jsonEvent(jsonStr), verbose: true
+        publishEvent event: jsonEvent(sprintf(jsonStr, "$EVENT_STR", "$POM_ARTIFACT", "$POM_GROUP", "$POM_VERSION", "$POM_PACKAGE")), verbose: true
     } catch (e) {
         echo "Publishing event failed.  Check that plugins are installed and configured correctly."
         throw e
