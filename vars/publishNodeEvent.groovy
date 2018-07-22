0d8a297aa77715ccd27c032e51cbaf698ddb5794
Brent McConnell <bmcconnell@cloudbees.com>
2018-07-22 08:17:59 -0400

    Create publishNodeEvent.groovy

---
+++ vars/publishNodeEvent.groovy
@@ -0,0 +1,28 @@
+def call() {
+    echo "Publishing event for Maven build"
+    try {
+        def json = readJSON file: 'package.json'
+        VERSION = json['version']
+        APP_NAME = json['name']
+       
+        echo "Building $APP_NAME"
+        echo "Building Version=$VERSION"
+        JSONSTR = """
+    {
+        "event":"$APP_NAME:$VERSION",
+        "appName": "$APP_NAME",
+        "version": "$VERSION"
+    }
+    """
+    } catch (e) {
+        echo "Cannot access package.json file"
+        throw e
+    }
+    
+    try {
+        publishEvent event: jsonEvent(JSONSTR), verbose: true
+    } catch (e) {
+        echo "Publishing event failed.  Check that plugins are installed and configured correctly."
+        throw e
+    }
+}
