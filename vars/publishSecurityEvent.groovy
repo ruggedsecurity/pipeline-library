1d8ad6c51e40c9bb05bb45893ed7eced654c27fe
Brent McConnell <bmcconnell@cloudbees.com>
2018-07-27 12:02:54 -0400

    Create publishSecurityEvent.groovy

---
+++ vars/publishSecurityEvent.groovy
@@ -0,0 +1,18 @@
+def call(hostName) {
+    echo "Publishing security event for Scan Failure"
+    try {
+        env.HOST_NAME = hostName
+       
+        echo "Failed Test for $APP_NAME"
+        JSONSTR = """
+        {
+            "event":"Security-Event",
+            "hostName": "$HOST_NAME"
+        }
+        """
+        publishEvent event: jsonEvent(JSONSTR), verbose: true
+    } catch (e) {
+        echo "Publishing security event failed.  Check that plugins are installed and configured correctly."
+        throw e
+    }
+}
