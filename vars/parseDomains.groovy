0be643e556d560a3eeeebf1dd925c0deb2f59903
Brent McConnell <emcconne@mindby.com>
2018-12-05 17:35:59 -0500

    Create parseDomains.groovy

---
+++ vars/parseDomains.groovy
@@ -0,0 +1,10 @@
+def parseConfig(data) {
+    // readJSON returns a non-serializable object, which we need to
+    // iterate over and store into a new list because pipelines require
+    // serializable data.
+    configs = []
+    for (el in data) {
+        configs << el
+    }
+    return configs
+}
